7411db510d85783870153b0a1cd0afe706ffeee3
Youssef Barakat <96824179+YoussefEmad02@users.noreply.github.com>
2023-06-04 15:19:36 +0300

    Add files via upload

---
+++ Phase 1/Reg.vhd
@@ -0,0 +1,54 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    16:59:09 04/26/2022 
+-- Design Name: 
+-- Module Name:    Reg - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity Reg is
+
+    Port ( input : in  STD_LOGIC_VECTOR (31 downto 0);
+           rst : in  STD_LOGIC;
+			  clk : in  STD_LOGIC;
+           load : in  STD_LOGIC;
+			  inc : in STD_LOGIC;
+           output : out  STD_LOGIC_VECTOR (31 downto 0));
+end Reg;
+
+architecture Behavioral of Reg is
+signal temp : std_logic_vector (31 downto 0); 	
+begin
+
+	process (clk, rst) 
+		begin
+			IF (rst = '1') THEN
+				temp <= (others => '0');
+			ELSIF (clk'event and clk = '1') THEN
+				IF(load ='1') THEN
+					temp <= input;
+				ELSIF (inc = '1')THEN
+					temp <= STD_LOGIC_VECTOR(signed(temp) + 1);
+				END IF;
+			END IF;
+		end process;
+
+	output <= temp;
+	
+end Behavioral;
+
---
+++ Phase 1/RegDef.vhd
@@ -0,0 +1,53 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    16:53:45 04/26/2022 
+-- Design Name: 
+-- Module Name:    RegDef - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity RegDef is
+
+    Port ( input : in  STD_LOGIC_VECTOR (31 downto 0);
+           rst : in  STD_LOGIC;
+			  clk : in  STD_LOGIC;
+           load : in  STD_LOGIC;
+			  increment : in STD_LOGIC;
+           output : out  STD_LOGIC_VECTOR (31 downto 0));
+end RegDef;
+
+architecture Behavioral of RegDef is
+signal temporary : std_logic_vector (31 downto 0); 	
+begin
+
+	process (clk, rst) 
+		begin
+			if (rst = '1') then
+				temporary <= (others => '0');
+			elsif (clk'event and clk = '1' and load = '1') then
+				temporary <= input;
+				elsif (clk'event and clk = '1' and increment = '1') then
+				temporary <= STD_LOGIC_VECTOR(signed(temporary) + 1);
+			
+			end if;
+		end process;
+	
+	output <= temporary;
+	
+end Behavioral;
+
